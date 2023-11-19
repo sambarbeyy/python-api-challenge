d0ad10452d633452409b19444110392feb5f981f
sambarbeyy <135924263+sambarbeyy@users.noreply.github.com>
2023-11-19 16:49:02 -0500

    Update api_keys.py

--- python-api-challenge/06 Starter_Code/starter_code/api_keys.py
+++ python-api-challenge/06 Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "7eae97d3d699e2df1fdf8b65207138f3"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ac69079139b2463e94cb6895e904911c"
